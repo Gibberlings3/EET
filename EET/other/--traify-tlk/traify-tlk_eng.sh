c1c3f17e733076f7f6821eab45f9fa165db43a39
Crevs Daak <crevs.daak@gmail.com>
2014-12-29 19:23:52 -0300

    add .sh files to --traify-tlk in Unix-based

---
+++ EET/other/--traify-tlk/traify-tlk_eng.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#this is meant to be run in ee/other/--traify-tlk/
+cd .. && cd .. && chmod 755 eet/weidu
+./eet/weidu --noautoupdate --no-auto-tp2 --logapp --out "bgee.tra" --use-lang "en_us" --traify-tlk
