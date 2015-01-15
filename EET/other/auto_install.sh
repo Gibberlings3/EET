b52d29f3f60efad8c3cf368398e020bf36417049
Crevs Daak <crevs.daak@gmail.com>
2015-01-15 20:31:30 -0300

    add auto_install option for *nix uses

---
+++ EET/other/auto_install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#this is meant to be run in eet/other/
+
+./weidu --force-install 0 --skip-at-view --language 0 --args-list p "/Applications/Baldur's Gate - Enhanced Edition/Game Data/00777/Baldur's Gate - Enhanced Edition.app/Contents/Resources"
