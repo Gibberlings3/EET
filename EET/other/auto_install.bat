125afe93a0c294cae14a9d055f76fef90404717a
K4thos <piotrpit.uek@gmail.com>
2015-01-16 14:00:06 +0100

    --args-list auto install option for EET_Tweaks

--- EET/other/auto_install.bat
+++ EET/other/auto_install.bat
@@ -1,1 +1,1 @@
-Setup-EET.exe --force-install 0 --skip-at-view --language 0 --args-list p "C:/Program Files (x86)/BeamDog/Games/00766"
+setup-EET.exe --force-install 0 --skip-at-view --language 0 --args-list p "C:/Program Files (x86)/BeamDog/Games/00766"
