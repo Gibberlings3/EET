22b858e7bf9c4aa0f22952f36f6576ccf62cd0e7
K4thos <piotrpit.uek@gmail.com>
2015-02-24 05:29:30 +0100

    cosmetic

--- EET_Tweaks/0/K#TELSPL.d
+++
@@ -1,9 +0,0 @@
-BEGIN ~K#TELSPL~
-
-IF ~  True()
-~ THEN BEGIN 0
-  SAY @1000500 /* ~Choose destination area:~ */
-  IF ~~ THEN REPLY @1000501 /* ~Stay in current area~ */ DO ~DestroySelf()
-~ EXIT
-  //IF ~~ THEN REPLY @2 GOTO 1
-END
