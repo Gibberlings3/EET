4b690f14ffb3bf1fa8c832d3cd7fe591c478beba
K4thos <piotrpit.uek@gmail.com>
2017-06-29 12:19:10 +0200

    Version 1.0 Release Candidate 8.7
    - new icons file (fixed compression problems, new Lava's icons)
    - Verr'Sza NPC support when installed on BG:EE
    - improved PIDs patching rules in eet_end component (fixes problem in Sandrah Saga)
    -WeiDU updated to v242

--- EET/other/auto_install.sh
+++ EET/other/auto_install.sh
@@ -2,4 +2,4 @@
 
 #this is meant to be run in eet/other/
 
-./weidu --force-install 0 --skip-at-view --language 0 --args-list p "/Applications/Baldur's Gate - Enhanced Edition/Game Data/00777/Baldur's Gate - Enhanced Edition.app/Contents/Resources"
+./weidu --force-install 0 --skip-at-view --language 0 --args-list p "/Applications/Baldur's Gate - Enhanced Edition/Game Data/00806/Baldur's Gate - Enhanced Edition.app/Contents/Resources"
