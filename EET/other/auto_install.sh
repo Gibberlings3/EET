4c60a99c73044a2ee5addf8fe5fa9b0136f359fa
K4thos <piotrpit.uek@gmail.com>
2017-08-14 18:57:28 +0200

    mac compatibility

--- EET/other/auto_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#this is meant to be run in eet/other/
-
-./weidu --force-install 0 --skip-at-view --language 0 --args-list p "/Applications/Baldur's Gate - Enhanced Edition/Game Data/00806/Baldur's Gate - Enhanced Edition.app/Contents/Resources"
